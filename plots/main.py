2fc1b5f9afb90b9e3f4955a811a2e47e3c7e9460
mrmalvicino <mrmalvicino@gmail.com>
2025-05-14 01:55:36 -0300

    :sparkles: feat: added `Fase lineal` section to chapter 2
    - Added the definitions and examples of linear phase systems.
    - Plots created with python locally

--- plots/main.py
+++ plots/main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import sys
 import os
 
@@ -6,9 +5,12 @@ base_path = os.path.abspath(os.path.join(os.getcwd(), "../../dsp-package"))
 sys.path.append(base_path)
 
 from dsp.signal import Signal
+from dsp.generator import Generator
 from dsp.grapher import Grapher
 from dsp.archive import Archive
 
+generator = Generator()
+generator.sampling_rate = 320000
 grapher = Grapher()
 archive = Archive()
 archive.output_path = "../src/images"
